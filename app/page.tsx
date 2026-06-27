da2b2dcc0ed17a6d1a214cb29ea1b2aed7455ed5
VINdicated <getvindicated@outlook.com>
2026-06-27 11:14:51 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { RoadScene } from "@/components/sections/RoadScene";
 import { HomeHero } from "@/components/sections/HomeHero";
-import { HomeDashboard } from "@/components/sections/HomeDashboard";
 import { HomeCards } from "@/components/sections/HomeCards";
 import { HomeQuote } from "@/components/sections/HomeQuote";
 import { HomeFounder } from "@/components/sections/HomeFounder";
